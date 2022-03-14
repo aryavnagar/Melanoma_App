6977d6a3aea7ebb01552aaf4481641b0854809bb
Aryav Nagar <aryavnagar@gmail.com>
2022-03-14 10:19:23 -0700

    Model code updated

--- Model.py
+++ Model.py
@@ -1,20 +1,24 @@
-
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, MaxPool2D, BatchNormalization
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
 from keras import backend as K
+from tensorflow import keras
 import autokeras as ak
 from PIL import Image 
 import numpy as np
 import time
 import math
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-NAME = "Melanoma-PennHackathon-Model-{}".format(int(time.time()))
+
+NAME = "Melanoma-Hackathon-Model{}".format(int(time.time()))
 tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 tf.random.set_seed(1234)
@@ -45,73 +49,7 @@ y_val = np.array(y_val, dtype=np.float32)
 y_test = np.array(y_test, dtype=np.float32)
 
 
-model = Sequential()
-model.add(Conv2D(512, (5, 5), activation="relu", input_shape=(224, 224, 3)))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2)))  
-model.add(Dropout(0.3))
-
-model.add(Conv2D(256, (3, 3),activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2)))  
-model.add(Dropout(0.2))
-
-model.add(Conv2D(256, (3, 3),activation='relu'))
-#model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2)))  
-model.add(Dropout(0.3))
-model.add(Flatten())
-
-model.add(Dense(128))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-# Train
-#You can also use generator to use augmentation during training.
-
-EPOCHS = 50
-BATCH_SIZE = 8
-TRAIN_SIZE = len(X_train)
-VAL_SIZE = len(X_val)
-
-compute_steps_per_epoch = lambda x: int(math.ceil(1. * x / BATCH_SIZE))
-
-steps_per_epoch = compute_steps_per_epoch(TRAIN_SIZE)
-val_steps = compute_steps_per_epoch(VAL_SIZE)
-
-history = model.fit(
-    X_train, y_train,
-    epochs=EPOCHS,
-    batch_size = BATCH_SIZE,
-    validation_data=(X_val, y_val),
-    verbose=1, callbacks=[tensorboard],
-    steps_per_epoch = steps_per_epoch,
-    validation_steps = val_steps)
-
-score = model.evaluate(X_test, y_test)
-print('Test accuracy:', score[1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-BATCH_SIZE = 8
+BATCH_SIZE = 64
 TRAIN_SIZE = len(X_train)
 VAL_SIZE = len(X_val)
 compute_steps_per_epoch = lambda x: int(math.ceil(1. * x / BATCH_SIZE))
@@ -119,33 +57,61 @@ compute_steps_per_epoch = lambda x: int(math.ceil(1. * x / BATCH_SIZE))
 steps_per_epoch = compute_steps_per_epoch(TRAIN_SIZE)
 val_steps = compute_steps_per_epoch(VAL_SIZE)
 
-clf = ak.ImageClassifier(max_trials=25)
-clf.fit(
-    X_train,
-    y_train,
-    validation_data=(X_val, y_val),
-    epochs=10,
-    batch_size=BATCH_SIZE,
-    steps_per_epoch = steps_per_epoch,
-    validation_steps = val_steps
-)
+# AutoKeras
+# clf = ak.ImageClassifier(max_trials=25)
+# clf.fit(
+#     X_train,
+#     y_train,
+#     validation_data=(X_val, y_val),
+#     epochs=10,
+#     batch_size=BATCH_SIZE,
+#     steps_per_epoch = steps_per_epoch,
+#     validation_steps = val_steps)
 
-#Evaluate the classifier on test data
-_, acc = clf.evaluate(X_test, y_test)
-print("Accuracy = ", (acc * 100.0), "%")
+# #Evaluate the classifier on test data
+# _, acc = clf.evaluate(X_test, y_test)
+# print("Accuracy = ", (acc * 100.0), "%")
 
-# get the final best performing model
-model = clf.export_model()
-print(model.summary())
+# # get the final best performing model
+# model = clf.export_model()
+# print(model.summary())
 
-_, acc = model.evaluate(X_test, y_test)
-print("Accuracy = ", (acc * 100.0), "%")
-
-
-#Save the model
-model.save('Melanoma_model.h5')
+# _, acc = model.evaluate(X_test, y_test)
+# print("Accuracy = ", (acc * 100.0), "%")
 
 
+# #Save the model
+# model.save('Melanoma_model.h5')
+
+# # Load the model
+# model = keras.models.load_model(r"D:\Github\Melanoma Hackathon\Best Models By Date\weights-improvement-02-0.92.hdf5")
+
+# #Model Checkpoint
+# save_filepath = r"D:\Github\Melanoma Hackathon\Best Models By Date\Saved_Models\weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+# checkpoint = ModelCheckpoint(save_filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+# # Early Stopping
+# early_stop = EarlyStopping(monitor='val_loss', patience=6, verbose=1)
+
+# #CSV logger (logs epoch, accuracy, val_accuracy, and val_loss)
+# log_filepath = r"D:\Github\Melanoma Hackathon\Best Models By Date\model_logs.csv"
+# log_csv = CSVLogger('log_filepath',separator=',', append=False)
+    
+# model.fit(
+#     X_train,
+#     y_train,
+#     validation_data=(X_val, y_val),
+#     epochs=50,
+#     batch_size=BATCH_SIZE,
+#     steps_per_epoch = steps_per_epoch,
+#     validation_steps = val_steps,
+#     callbacks=[tensorboard, checkpoint, log_csv, early_stop]
+# )
+
+# model.save()
+
+
+model = keras.models.load_model(r"D:\Github\Melanoma Hackathon\Best Models By Date\weights-improvement-02-0.92.hdf5")
 #model.summary easy to view
 import pandas as pd
 table=pd.DataFrame(columns=["Name","Type","Shape"])
@@ -153,9 +119,6 @@ for layer in model.layers:
     table = table.append({"Name":layer.name, "Type": layer.__class__.__name__,"Shape":layer.output_shape}, ignore_index=True)
 
 table
-
-
-
 
 
 
